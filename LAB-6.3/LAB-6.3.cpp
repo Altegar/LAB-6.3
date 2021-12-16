b867e3454daaf10c971d4615ea040e10b11bcb72
Sushomeister <91095463+Sushomeister@users.noreply.github.com>
2021-12-16 13:13:39 +0200

    Update LAB-6.3.cpp

--- LAB-6.3/LAB-6.3.cpp
+++ LAB-6.3/LAB-6.3.cpp
@@ -7,8 +7,16 @@
 // Ітераційний спосіб
 
 #include <iostream>
+#include <iomanip>
+#include <ctime>
 
 using namespace std;
+
+void Random(int* a, int n, int x, int y)
+{
+	for (int i = 0; i < n; i++) // i - додаткова змінна
+		a[i] = rand() % (y - x + 1) + x;
+}
 
 int Max(int* a, const int n)
 {
@@ -16,14 +24,26 @@ int Max(int* a, const int n)
 	for (int i = 1; i < n; i++)
 		if (a[i] > max)
 			max = a[i];
-
 	return max;
+}
+
+void Print(int* a, int n) // форматне виведення
+{
+	for (int i = 0; i < n; i++)
+		cout << setw(3) << a[i] << " ";
+	cout << "\n";
 }
 
 int main()
 {
-	const int n = 10;
-	int a[n] = { 1,3,2,6,15,27,9,61,33,42 };
+	srand((unsigned)time(0)); // ініціалізація генератора випадкових чисел
+
+	const int n = 24;
+	int a[n];
+	int x = 2, y = 66;
+
+	Random(a, n, x, y);
+	cout << "Array = "; Print(a, n);
 
 	cout << "Max = " << Max(a, n) << endl;
 
